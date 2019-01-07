c09eba098d6036b5c6833d147679946718c57e9b
Walaryne <hazardoustheradioactive@gmail.com>
2019-01-07 17:44:43 -0500

    Made formatting changes, fixed various pieces of code

--- commands/who-is.js
+++ commands/who-is.js
@@ -3,13 +3,8 @@ module.exports = {
     description: 'gets info on a specified user',
     aliases: ["user", "userinfo"],
     cooldown: 2,
-    execute(message, args) 
-    {
-        const Discord = require('discord.js');
-        const client = new Discord.Client();
-
-        client.fetchUser(message.content.slice(7), false).then(function(user) 
-        {
+    execute(message, args, client) {
+        client.fetchUser(args[0], false).then(function(user) {
             message.channel.send(user.username);
             message.channel.send('Joined: ' + user.createdAt);
             message.channel.send(user.avatarURL);
--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const fs = require('fs'); // Imports fs, NodeJS File System - required for dynam
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
-const cooldowns = new Discord.Collection(); 
-//const prefix = require('./config.json').prefix; // Declares constant variable with prefix as value
-
-//NOTE: Keep the above statement commented out UNTIL the file contains valid JSON
-//Ryn, it does - not sure what's going on here --Ben
+const cooldowns = new Discord.Collection();
+const prefix = require('./config.json').prefix; // Declares constant variable with prefix as value
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -16,23 +13,21 @@ const cooldowns = new Discord.Collection();
 client.on('ready', () => {
     console.log('I am ready!');
     client.user.setGame('with itself'); // Would advise swapping to setActivity to avoid redundancy  --Ben
+    //Planning on fixing this Ben, it's a deprecated function btw, not a redundancy.
 });
 
 client.commands = new Discord.Collection(); // Creates an instance of the Command collection from discord.js
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); // takes every .js file from the folder and loads it into an array
 
-for (const file of commandFiles) // Runs through the aforementioned array and imports all of the files as modules.
-{
+for (const file of commandFiles) { // Runs through the aforementioned array and imports all of the files as modules.
     const command = require(`./commands/${file}`); // assigns file x to command
     client.commands.set(command.name, command); // assigns command module to the discord command set
 }
 
 // Create an event listener for messages
-client.on('message', message =>
-{
+client.on('message', message => {
     //#region multi-word-commands
-    switch (message.content)
-    {
+    switch (message.content) {
         case 'All the other kids...':
             message.channel.send('with the pumped up kicks...');
             break;
@@ -58,35 +53,30 @@ client.on('message', message =>
     //#region parser+command-handler
     if (!message.content.startsWith(prefix) || message.author.bot) return; // Disregards message if it does not begin with the command prefix and that the user is not a bot
 
-    
-    const msgArgs = message.content.slice(prefix.length).split(/ +/);// argumentss becomes an array containing every word after the initial command
+
+    const msgArgs = message.content.slice(prefix.length).split(/ +/); // argumentss becomes an array containing every word after the initial command
     const commandName = msgArgs.shift().toLowerCase() // The initial command has the prefix removed and is shifted to lower case, and is assigned to commandName
 
-    if (!client.commands.has(commandName) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))) // Checks to see if the command exists / alias exists
-    {
-        message.reply(`🤔 I-I'm not sure what ${command} means ;-;`); 
+    if (!client.commands.has(commandName) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))) { // Checks to see if the command exists / alias exists
+        message.reply(`🤔 I-I'm not sure what ${command} means ;-;`);
         return;
-    } 
+    }
 
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName)); // retrieves actual command from module
     if (!command) return; // Uses psuedotruth to return on null value
 
-    if (!cooldowns.has(command.name)) // Checks to see if the command is on cooldown
-    {
-        cooldowns.set(command.name, new Discord.Collection()); // Adds the command to the cooldowns object 
+    if (!cooldowns.has(command.name)) { // Checks to see if the command is on cooldown
+        cooldowns.set(command.name, new Discord.Collection()); // Adds the command to the cooldowns object
     }
-    
+
     const now = Date.now(); // gets current time to initiate the cooldown
     // Taken from the discord documentation, I don't follow but my tests indicate it works
-    const timestamps = cooldowns.get(command.name); 
+    const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 3) * 1000;
 
-    if (timestamps.has(message.author.id)) 
-    {
+    if (timestamps.has(message.author.id)) {
         const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-        if (now < expirationTime) 
-        {
+        if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
             return message.reply(`AAAAAAAAAAA! Too fast! Give me ${timeLeft.toFixed(1)} seconds before running \`${command.name}\` again!.`);
         }
@@ -97,9 +87,8 @@ client.on('message', message =>
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
-        command.execute(message, args); // actually executes the command
-    }
-    catch (error) {
+        command.execute(message, args, client); // actually executes the command
+    } catch (error) {
         console.error(error);
         message.reply(`Uwaaa! I think I dropped something...`);
     }
@@ -107,7 +96,7 @@ client.on('message', message =>
 
     //#region RedundantCode
     /*try {             // ALL OF THIS CODE IS REDUNDANT BUT KEPT UNTIL MY OWN IS PROVEN TO BE ROBUST
-        
+
         if (message.content.startsWith('%whois')) {
             client.fetchUser(message.content.slice(7), false).then(function(user) {
                 message.channel.send(user.username);
@@ -145,8 +134,8 @@ client.on('message', message =>
     } catch (err) {
         console.log(err);
     }*/
-//#endregion
-}); 
+    //#endregion
+});
 
 client.on('guildCreate', guild => {
     try {
